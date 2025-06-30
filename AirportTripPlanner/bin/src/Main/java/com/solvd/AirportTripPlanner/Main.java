16f36d53ab0634907bce93477326b2774f0bddad
Vitalii82 <svinovej90@gmail.com>
2025-06-30 16:58:26 +0300

    refactored environment

---
+++ AirportTripPlanner/bin/src/Main/java/com/solvd/AirportTripPlanner/Main.java
@@ -0,0 +1,25 @@
+package AirportTripPlanner;
+
+import AirportTripPlanner\bin\src\Main\java\com\solvd\AirportTripPlanner.model;
+import AirportTripPlanner\bin\src\Main\java\com\solvd\AirportTripPlanner.logic;
+import AirportTripPlanner\bin\src\Main\java\com\solvd\AirportTripPlanner.interfaces;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Airport from = new Airport("JFK", "John F. Kennedy International Airport");
+        Airport to = new Airport("LAX", "Los Angeles International Airport");
+
+        Flight flight1 = new Flight("AA100", from, to, 300.0, 360);
+        from.addFlight(flight1);
+
+        Passenger passenger = new Passenger("Vitalii", "Svinovei");
+        Baggage baggage = new Baggage(20.0);
+
+        Notifiable notifier = new NotificationService();
+        TravelService travelService = new TravelService(notifier);
+
+        travelService.planTrip(passenger, from, to, baggage);
+    }
+}
